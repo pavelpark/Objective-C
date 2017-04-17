31e95ee291792293354d2225d893ee7485ff00b2
Pavel Parkhomey <pavelpark@aol.com>
2017-04-17 13:17:21 -0700

    Got the Setter Set up

---
+++ Objective-C/Objective-C/Employee.h
@@ -0,0 +1,19 @@
+//
+//  Employee.h
+//  Objective-C
+//
+//  Created by Pavel Parkhomey on 4/17/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "Person.h"
+
+@interface Employee : Person
+
+//Getter
+-(int)employeeNumber;
+-(int)yearsEmployed;
+-(NSString *)managerName;
+
+
+@end
---
+++ Objective-C/Objective-C/Employee.m
@@ -0,0 +1,26 @@
+//
+//  Employee.m
+//  Objective-C
+//
+//  Created by Pavel Parkhomey on 4/17/17.
+//  Copyright © 2017 Pavel Parkhomey. All rights reserved.
+//
+
+#import "Employee.h"
+
+@implementation Employee
+
+int employeeNumber;
+int yearsEmployed;
+NSString *managerName;
+
+-(int)employeeNumber {
+    return employeeNumber;
+}
+-(int)yearsEmployed {
+    return yearsEmployed;
+}
+-(NSString *)managerName {
+    return managerName;
+}
+@end
